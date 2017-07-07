92fdbc89a0d676116aa63d7888fa0803ce434d4b
Sunandita Patra <patras@Sunanditas-MacBook-Pro.local>
2017-07-08 00:02:54 +0200

    Adding code

---
+++ rae1.py
@@ -0,0 +1,229 @@
+from __future__ import print_function
+"""
+File rae1.py
+Author: Dana Nau <nau@cs.umd.edu>, July 7, 2017
+This is a simplified version of Rae that has just one execution stack.
+"""
+
+import sys, pprint
+
+############################################################
+# States and goals
+
+class State():
+	"""A state is just a collection of variable bindings."""
+	def __init__(self):
+		pass
+
+### A goal is identical to a state except for the class name.
+### I don't know if we'll need it or not, but it was trivial to write,
+### and it might be useful if we want to reason about goals.
+
+class Goal():
+	"""A goal is just a collection of variable bindings."""
+	def __init__(self):
+		pass
+
+
+def print_state(state):
+	"""Print each variable in state, indented by indent spaces."""
+	if state != False:
+		for (name,val) in vars(state).items():
+			print('| '*indent + '  state.' + name + ' =', val)
+	else: print('False')
+
+### Print_goal is identical to print_state except for the word 'goal'
+
+def print_goal(goal):
+	"""Print each variable in goal, indented by indent spaces."""
+	if goal != False:
+		for (name,val) in vars(goal).items():
+			print('| '*indent + '	goal.' + name + ' =', val)
+	else: print('False')
+
+### for debugging
+
+verbose = 0
+
+
+def verbosity(level):
+	"""
+	Specify how much debugging printout to produce:
+	
+	verbosity(0) makes Rae1 run silently; the only printout will be
+	whatever the domain author has put into the commands and methods.
+	
+	verbosity(0) makes Rae1 print messages at the start and end, and
+	print the name and args of each task and command.
+	
+	verbosity(0) makes Rae1 print a lot of stuff.
+	"""
+	
+	global verbose
+	verbose = level
+
+############################################################
+# Helper functions that may be useful in domain models
+
+def forall(seq,cond):
+	"""True if cond(x) holds for all x in seq, otherwise False."""
+	for x in seq:
+		if not cond(x): return False
+	return True
+
+def find_if(cond,seq):
+	"""
+	Return the first x in seq such that cond(x) holds, if there is one.
+	Otherwise return None.
+	"""
+	for x in seq:
+		if cond(x): return x
+	return None
+
+############################################################
+# Functions to tell Rae1 what the commands and methods are
+
+commands = {}
+methods = {}
+
+def declare_commands(*cmd_list):
+	"""
+	Call this after defining the commands, to tell Rae1 what they are. 
+	cmd_list must be a list of functions, not strings.
+	"""
+	commands.update({cmd.__name__:cmd for cmd in cmd_list})
+	return commands
+
+def declare_methods(task_name,*method_list):
+	"""
+	Call this once for each task, to tell Rae1 what the methods are.
+	task_name must be a string.
+	method_list must be a list of functions, not strings.
+	"""
+	methods.update({task_name:list(method_list)})
+	return methods[task_name]
+
+############################################################
+# The user can use these to see what the commands and methods are.
+
+def print_commands(olist=commands):
+	"""Print out the names of the commands"""
+	print('commands:', ', '.join(olist))
+
+def print_methods(mlist=methods):
+	"""Print out a table of what the methods are for each task"""
+	print('{:<14}{}'.format('TASK:','METHODS:'))
+	for task in mlist:
+		print('{:<14}'.format(task) + ', '.join([f.__name__ for f in mlist[task]]))
+
+############################################################
+# Stuff for debugging printout
+
+indent = 0
+indent_increment = 1
+
+class Failed_command(Exception):
+	pass
+
+class Failed_task(Exception):
+	pass
+
+class Incorrect_return_code(Exception):
+	pass
+
+############################################################
+# The actual planner
+
+def rae1(task,*args):
+	"""
+	Rae1 is the Rae actor with a single execution stack. The first argument is the name (which
+	should be a string) of the task to accomplish, and args[0:-1] are the arguments for the task. 
+	The last argument, args[-1], must be the initial state.
+	"""
+	global indent
+	indent = indent_increment
+	if verbose>0: 
+		print('\nRae1: verbose={}, task {}{}'.format(verbose,task,args[0:-1]))
+	if verbose>0:
+		print('| '*indent + 'Initial state is:')
+		print_state(args[-1])
+	try:
+		retcode = do_task(task,*args)
+	except Failed_command,e:
+		if verbose>0: 
+			print('| '*indent + 'Failed command {}'.format(e))
+		retcode = 'Failure'
+	except Failed_task,e:
+		if verbose>0: 
+			print('| '*indent + 'Failed task {}'.format(e))
+		retcode = 'Failure'
+	else:
+		pass
+	if verbose>0:
+		print('| '*indent + 'Final state is:')
+		print_state(args[-1])
+	return retcode
+
+def choose_candidate(candidates):
+	return(candidates[0],candidates[1:])
+
+def do_task(task,*args):
+	"""
+	This is the workhorse for rae1. THe arguments are the same except that arg[-1] is the 
+	current state (e.g., after some commands have been executed).
+	"""
+	global indent
+	
+	if verbose>0: 
+		print('| '*indent + 'Begin task {}{}'.format(task,args[0:-1]))
+	indent = indent + indent_increment
+	retcode = 'Failure'
+	candidates = methods[task]
+	while (retcode == 'Failure' and candidates != []):
+		(m,candidates) = choose_candidate(candidates)
+		if verbose>1:
+			print('| '*indent + 'Try method {}{}'.format(m.__name__,args[0:-1]))
+		try:
+			retcode = m(*args)
+		except Failed_command,e:
+			if verbose>0: 
+				print('| '*indent + 'Failed command {}'.format(e))
+			retcode = 'Failure'
+		except Failed_task,e:
+			if verbose>0: 
+				print('| '*indent + 'Failed task {}'.format(e))
+			retcode = 'Failure'
+		else:
+			pass
+		if verbose>1: 
+			print('| '*indent + '{} for method {}{}'.format(retcode,m.__name__,args[0:-1]))
+	
+	indent = indent - indent_increment
+	if retcode == 'Failure':
+		raise Failed_task('{}{}'.format(task, args[0:-1]))
+	elif retcode == 'Success':
+		return retcode
+	else:
+		raise Incorrect_return_code('{} for {}{}'.format(retcode, task, args[0:-1]))
+
+
+def do_command(cmd,*args):
+	"""
+	Perform command cmd(args). Last arg must be the current state
+	"""
+	global indent
+	
+	indent = indent + indent_increment
+	if verbose>0: 
+		print('| '*indent + 'Begin command {}{}'.format(cmd.__name__,args[0:-1]))
+	retcode = cmd(*args)
+	if verbose>1: 
+		print('| '*indent + '{}, current state is:'.format(retcode))
+		print_state(args[-1])
+	indent = indent - indent_increment
+	if retcode == 'Failure':
+		raise Failed_command('{}{}'.format(cmd.__name__, args[0:-1]))
+	elif retcode == 'Success':
+		return retcode
+	else:
+		raise Incorrect_return_code('{} for {}{}'.format(retcode, cmd.__name__, args[0:-1]))
---
+++ ste.py
@@ -0,0 +1,138 @@
+from __future__ import print_function
+"""
+File ste.py
+Author: Dana Nau <nau@cs.umd.edu>, July 7, 2017
+Based roughly on the "travel from home to the park" example in my lectures,
+but modified to have multiple levels of tasks.
+"""
+
+import copy
+import rae1
+
+def taxi_rate(dist):
+	return (1.5 + 0.5 * dist)
+
+def walk(a,x,y,state):
+	if state.loc[a] == x:
+		print('agent',a,'walks from',x,'to',y)
+		state.loc[a] = y
+		return 'Success'
+	else:
+		print('agent',a,"isn't at location",x)
+	return 'Failure'
+
+def call_taxi(a,x,state):
+	print('a taxi appears at location',x)
+	state.loc['taxi'] = x
+	return 'Success'
+
+def enter_taxi(a,state):
+	if state.loc['taxi'] == state.loc[a]:
+		print('agent',a,'enters taxi at location',state.loc[a])
+		state.loc[a] = 'taxi'
+		return 'Success'
+	else:
+		print("there's no taxi for agent",a,'to enter')
+		return 'Failure'
+
+def taxi_carry(a,y,state):
+	if state.loc[a]=='taxi':
+		x = state.loc['taxi']
+		print('taxi carries agent',a,'from',x,'to',y)
+		state.loc['taxi'] = y
+		state.owe[a] = taxi_rate(state.dist[x][y])
+		return 'Success'
+	else:
+		print('agent',a,"isn't in a taxi")
+		return 'Failure'
+
+def pay_driver(a,state):
+	if state.cash[a] >= state.owe[a]:
+		print('agent',a,'pays',state.owe[a],'to the taxi driver')
+		state.cash[a] = state.cash[a] - state.owe[a]
+		state.owe[a] = 0
+		return 'Success'
+	else:
+		print('agent',a,'cannot pay',state.owe[a],'to the taxi driver')
+		return 'Failure'
+
+def leave_taxi(a,state):
+	if state.loc[a]=='taxi':
+		print('agent',a,'leaves taxi at location',state.loc['taxi'])
+		state.loc[a] = state.loc['taxi']
+		return 'Success'
+	else:
+		print('agent',a,"isn't in a taxi")
+		return 'Failure'
+
+rae1.declare_commands(walk, call_taxi,enter_taxi,taxi_carry,pay_driver,leave_taxi)
+print('')
+rae1.print_commands()
+
+
+
+def travel_by_foot(a,x,y,state):
+	if state.dist[x][y] <= 2:
+		rae1.do_command(walk,a,x,y,state)
+		return 'Success'
+	return 'Failure'
+
+def travel_by_taxi(a,x,y,state):
+	if state.cash[a] >= taxi_rate(state.dist[x][y]):
+		rae1.do_command(call_taxi,a,x,state)
+		rae1.do_task('ride_taxi',a,y,state)
+		return 'Success'
+	else:
+		print('agent',a,"has too little money for a taxi from",x,'to',y)
+		return 'Failure'
+
+rae1.declare_methods('travel',travel_by_foot,travel_by_taxi)
+
+def ride_taxi_method(a,y,state):
+	if state.dist[state.loc[a]][y] < 50:
+		rae1.do_command(enter_taxi,a,state)
+		rae1.do_command(taxi_carry,a,y,state)
+		rae1.do_command(pay_driver,a,state)
+		rae1.do_command(leave_taxi,a,state)
+		return 'Success'
+	else:
+		print('the taxi driver is unwilling to drive to',y)
+		return 'Failure'		
+
+rae1.declare_methods('ride_taxi',ride_taxi_method)
+
+print('')
+rae1.print_methods()
+
+print('\n*********************************************************')
+print("* Call rae1 on several problems using verbosity level 1.")
+print("* For a different amout of printout, try 0 or 2 instead.")
+print('*********************************************************')
+
+rae1.verbosity(1)
+
+state = rae1.State()
+state.loc = {'me':'home'}
+state.cash = {'me':20}
+state.owe = {'me':0}
+state.dist = {'home':{'park':8}, 'park':{'home':8}}
+
+rae1.rae1('travel','me','home','park',state)
+
+
+state = rae1.State()
+state.loc = {'me':'home'}
+state.cash = {'me':5}
+state.owe = {'me':0}
+state.dist = {'home':{'park':8}, 'park':{'home':8}}
+
+rae1.rae1('travel','me','home','park',state)
+
+
+state = rae1.State()
+state.loc = {'me':'home'}
+state.cash = {'me':100}
+state.owe = {'me':0}
+state.dist = {'home':{'park':80}, 'park':{'home':80}}
+
+rae1.rae1('travel','me','home','park',state)
