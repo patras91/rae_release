14237354be79834b4e204ac7f169b2328252bcef
jmason12 <jmason12@terpmail.umd.edu>
2018-11-12 21:53:35 -0500

    Fixed bugs, problem1 now runs

--- shared/domains/env_packageDelivery.py
+++ shared/domains/env_packageDelivery.py
@@ -6,6 +6,11 @@ from domain_constants import *
 
 commandProb = {
     'lookupDB': [0.98, 0.02],
+    'moveRobot': [0.97, 0.03],
+    'wrap': [0.97, 0.03],
+    'pickup': [0.95, 0.05],
+    'load': [0.99, 0.01],
+    'groundShip': [0.99, 0.01]
 }
 
 def Sense(cmd):
