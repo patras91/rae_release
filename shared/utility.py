71828b9381451d8be97e4459b9ba4e0cb6b99bea
Sunandita Patra <>
2019-06-25 15:15:14 -0400

    fixing GLOBALS and utilities

---
+++ shared/utility.py
@@ -0,0 +1,85 @@
+__author__ = 'patras'
+import GLOBALS
+
+class Utility():
+    def __init__(self, val):
+        if GLOBALS.GetOpt() == 'min':
+            if val == 'Success':
+                self.value = 0
+            elif val == 'Failure':
+                self.value = float("inf")
+            else:
+                self.value = val    
+        else:
+            if val == 'Failure':
+                self.value = 0
+            elif val == 'Success':
+                self.value = float("inf")
+            else:
+                self.value = val
+
+    def __gt__(self, other): 
+        if self.value == 'UNK':
+            return False
+        if self.value > other.value: 
+            return True
+        else: 
+            return False
+
+    def __ge__(self, other): 
+        if self.value >= other.value: 
+            return True
+        else: 
+            return False
+
+    def __lt__(self, other):
+        if self.value < other.value: 
+            return True
+        else: 
+            return False
+
+    def __le__(self, other):
+        if self.value <= other.value: 
+            return True
+        else: 
+            return False
+
+    def __eq__(self, other):
+        if self.value == other.value:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        return str(self.value)
+
+    def __add__(self, other):
+        e1 = self.value
+        e2 = other.value
+        if e1 == float("inf"):
+            res = e2
+        elif e2 == float("inf"):
+            res = e1
+        elif e1 == 0 and e2 == 0:
+            res = 0
+        else:
+            res = e1 * e2 / (e1 + e2)
+        return Utility(res)
+
+    def SetValue(self, val):
+        self.value = val
+
+    def GetValue(self):
+        return self.value
+
+if __name__=="__main__":
+
+    a = Utility(1)
+    b = Utility(5)
+
+    print(a>b)
+    print(a>=b)
+    print(a<b)
+    print(a<=b)
+    print(a==b)
+
