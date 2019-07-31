18f6039cf0ec36a1af8c078cd7dfb86d5621b7a8
Sunandita Patra <sunandita.patra@gmail.com>
2019-07-31 02:59:16 -0400

    change in the test script of OF

--- test_scripts/AIJ/scripts/RAEplan/test_RAEplan_OF_UCT.bash
+++ test_scripts/AIJ/scripts/RAEplan/test_RAEplan_OF_UCT.bash
@@ -15,10 +15,10 @@ P=(
 "problem8"
 "problem9"
 )
-uct=("100" "300" "500" "700" "900") 
+uct=("50" "100" "300" "500" "700") 
 for problem in ${P[@]}
 do
-    for uct in ${uct[@]}
+    for uctCount in ${uct[@]}
     do
             setup="
 import sys
@@ -27,19 +27,19 @@ sys.path.append('../../../../shared/domains/')
 sys.path.append('../../../../shared/problems/OF')
 sys.path.append('../../../../shared/')
 from testRAEandRAEplan import GLOBALS, testBatch
-GLOBALS.SetUCTRuns($uct)
+GLOBALS.SetUCTRuns($uctCount)
 GLOBALS.SetOpt('max')
 GLOBALS.SetUCTmode('UCT')
 GLOBALS.SetSearchDepth(float(\"inf\"))"
 counter=1
 while [ $counter -le $runs ]
 do
-            echo $domain $problem "uct = " $uct ", Run " $counter/$runs
+            echo $domain $problem "uct = " $uctCount ", Run " $counter/$runs
             time_test="testBatch(domain='$domain', problem='$problem', useRAEplan=True)"
 
-            fname="../../results/${domain}_v_journal/rae_plan_uct_${uct}.txt" # You should have a folder called OF in the current folder
+            fname="../../results/${domain}_v_journal/rae_plan_uct_${uctCount}.txt" # You should have a folder called OF in the current folder
 
-            echo "Time test of $domain $problem $uct" >> $fname
+            echo "Time test of $domain $problem $uctCount" >> $fname
             python3 -m timeit -n 1 -r 1 -s "$setup" "$time_test" >> $fname
 ((counter++))
 done
