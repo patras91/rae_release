c65d2f1a9d7bb9a7c89eb68edcd94b630fe8787a
Sunandita Patra <sunandita.patra@gmail.com>
2019-09-19 15:06:43 -0400

    environment in SDN

---
+++ shared/domains/__pycache__/env_airsSDN.py
@@ -0,0 +1,18 @@
+import numpy
+from domain_constants import *
+from state import state, rv
+import GLOBALS
+
+commandProb = {
+    'applyRestart': [0.90, 0.1],
+    'applyAlternative': [0.80, 0.20],
+}
+
+def Sense(cmd):
+    p = commandProb[cmd]
+    outcome = numpy.random.choice(len(p), 50, p=p)
+    res = outcome[0]
+    if res == 0:
+        return SUCCESS
+    else:
+        return FAILURE
